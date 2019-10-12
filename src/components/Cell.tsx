239925203540b850c2d1d8b4bde429ae82f8e05e
Jasper Stam <jasper@strnk.nl>
2019-10-12 13:07:43 +0200

    Add move preview and remove editing

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,17 +1,28 @@
-import React, { useContext, useCallback } from 'react';
-import styled from 'styled-components';
+import React, { useContext, useCallback, useState } from 'react';
+import styled, { css } from 'styled-components';
 import { GameContext } from '../App';
 import { TicTacValue } from '../store/Game';
 import { observer } from 'mobx-react-lite';
 
+interface TicProps {
+  editable: boolean;
+}
 
-const Tic = styled.button`
+const Tic = styled.button<TicProps>`
   border: none;
   outline: none;
   background: white;
   font-size: 5rem;
   height: 200px;
   width: 200px;
+
+  ${props =>
+    props.editable &&
+    css`
+      &:hover {
+        opacity: 0.5;
+      }
+    `}
 `;
 
 interface CellProps {
@@ -19,14 +30,27 @@ interface CellProps {
   index: number;
 }
 
-const Cell : React.FC<CellProps> = ({ children, index}) => {
-
+const Cell: React.FC<CellProps> = ({ children: value, index }) => {
   const game = useContext(GameContext);
+  const editable = value === null;
+
+  const [hovering, setHovering] = useState(false);
+
   const handleClick = useCallback(() => {
-    game.set(index)
+    game.set(index);
   }, [game, index]);
+
+  const hoverValue = hovering ? game.currentPlayer : null;
+
   return (
-    <Tic onClick={handleClick}>{children}</Tic>
+    <Tic
+      onClick={handleClick}
+      editable={editable}
+      onMouseOver={() => setHovering(true)}
+      onMouseOut={() => setHovering(false)}
+    >
+      {value || hoverValue}
+    </Tic>
   );
 };
 
